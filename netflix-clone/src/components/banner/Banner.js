81b5df0f0a36d3dae3e30f62e8873215a60ee0bb
yonasenyew21-rgb <yonasenyew21@gmail.com>
2025-12-07 17:01:43 -0500

    created banner

---
+++ netflix-clone/src/components/banner/Banner.js
@@ -0,0 +1,37 @@
+import React from "react";
+import axios from "../../utils/axios";
+import requests from "../../utils/requests";
+import { useEffect, useState } from "react";
+import "./Banner.css";
+
+const Banner = () => {
+  const [movie, setMovie] = useState(null);
+  useEffect(() => {
+    const fetchBanner = async () => {
+      const response = await axios.get(requests.fetchNetflixOriginals);
+      const results = response.data.results;
+      setMovie(results[Math.floor(Math.random() * results.length)]);
+    };
+    fetchBanner();
+    console.log(movie);
+  }, []);
+
+  return movie ? (
+    <header
+      className="banner"
+      style={{
+        backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie?.backdrop_path})`,
+      }}
+    >
+      <div className="banner__content">
+        <h1 className="banner__title">
+          {movie.title || movie.name || movie.original_name}
+        </h1>
+        <p className="banner__description">{movie.overview}</p>
+      </div>
+      <div className="banner__fadeBottom"></div>
+    </header>
+  ) : null;
+};
+
+export default Banner;
--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
+import Banner from '../../components/banner/Banner'
 
 const home = () => {
   return (
     <>
       <Header/>
+      <Banner/>
       <Footer/>
+
     </>
   )
 }
